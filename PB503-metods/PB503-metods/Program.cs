d9dc9e47d13a393c5db6e6f3dfd3d561f3379ccc
Zumrud005 <mustafazuzu05@gmail.com>
2024-10-26 01:22:57 +0400

    tasks and searching

---
+++ PB503-metods/PB503-metods/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace PB503_metods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Bir int parametr qəbul edən və o ədədə qədər bütün cüt ədədlərin cəmini ekrana yazdıran, 
+            // həm return type - ı olan bir method, həm də void bir method yazırsız
+            Console.WriteLine("verilen eded qeder  cut ededlerin cemi:");
+            Console.WriteLine(NumberSum(11));
+
+
+            NumberSum2(19);
+
+            string name = " salam  necesen ";
+            
+            Console.WriteLine(RemoveSpace(name));
+
+
+            //Parametr olaraq 1 string dəyər qəbul edən və həmin string dəyəri icində bosluqlar qalmayacaq 
+            //hala gətirən metod.Misal olaraq, name = "  Code  Acacemy   " deyə bir variable - mız varsa onu 
+            //yaratdigimiz metoda göndərdikdə variable-daki dəyər "CodeAcademy" olmalıdır.
+
+
+
+
+        }
+
+
+        static int NumberSum(int a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+
+        static void NumberSum2(int b)
+        {
+            int sum2 = 0;
+            for (int i = 0; i < b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum2 = sum2 + i;
+                }
+            }
+            Console.WriteLine($"{b} ededine kimi cut ededlerin cemi= {sum2}");
+        }
+
+
+
+        static string RemoveSpace(string words)
+        {
+            string updateWord = "";
+            foreach (var wrd in words)
+            {
+                if (wrd != ' ')
+                {
+                    updateWord = updateWord + wrd;
+                }
+            }
+            return updateWord;
+
+
+        }
+
+
+
+    }
+}
---
+++ exponantial searching/exponantial searching/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class ExponentialSearch
+{
+        static void Main()
+        {
+            int[] array = { 10,23,25,45,89,96,102,120,690,847,921 };
+            int target = 102;
+
+            int result = ExponentiaSearch(array, target);
+
+            if (result != -1)
+                Console.WriteLine("ededin indeksi: " + result);
+            else
+                Console.WriteLine("eded tapılmadı.");
+        }
+
+    static int BinarySearch(int[] array, int left, int right, int target)
+    {
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+
+            if (array[mid] == target)
+                return mid;
+
+            if (array[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+        return -1;
+
+
+    }
+        static int ExponentiaSearch(int[] array, int target)
+        {
+            int n = array.Length;
+
+            if (array[0] == target)
+                return 0;
+
+
+            int i = 1;
+            while (i < n && array[i] <= target)
+                i *= 2;
+
+
+            return BinarySearch(array, i / 2, Math.Min(i, n - 1), target);
+        }
+}
